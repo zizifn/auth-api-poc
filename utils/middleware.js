13549ca403b61cdfadf7ffde4813dadae8041b5a
zizifn <515868058@qq.com>
2021-12-06 00:47:08 +0800

    init oauth2 api

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,5 @@
 const jwt = require('express-jwt');
+const winston = require('winston');
 const { AUTH0_API_SIGN_SECRET, AUTH0_API_AUDIENCE, AUTH0_API_SIGN_ISSUER } = require('../config/config.js');
 
 const auth0JWTCheck = jwt({
@@ -8,6 +9,15 @@ const auth0JWTCheck = jwt({
     algorithms: ['HS256']
 });
 
+const winstonLogger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console()
+    ],
+});
+
 module.exports = {
-    auth0JWTCheck
+    auth0JWTCheck,
+    winstonLogger
 }
